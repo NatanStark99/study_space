6f767558f0a266edf790d9c5b5fae01ef305fa53
NatanStark99 <natan.silveira999@gmail.com>
2025-07-27 21:42:42 -0300

    Atualizando projeto

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "categoria")
+    list_display = ("id", "nome", "legenda", "categoria", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome", "categoria")
-
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
